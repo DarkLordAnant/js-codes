d93e64aa750e74564be7850c03b25a98e795be7b
DarkLordAnant <97455369+DarkLordAnant@users.noreply.github.com>
2024-06-15 17:15:38 +0530

    Added initial codes

---
+++ js- hindi/datatypes.js
@@ -0,0 +1,7 @@
+"use strict"
+
+// alert("Hello")
+
+console.log(3+3)
+
+console.log(typeof null)
---
+++ js- hindi/test.js
@@ -0,0 +1,19 @@
+"use strict"
+// alert(3+3) We are using node.js and not browser so doesnt work
+
+console.log(3+3);
+console.log('3+3');
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+
+object
+
+*/
